ba891edabc6a0d4e74c6cb840c79bbf601c0cd91
AvipshaUSA <68029807+AvipshaUSA@users.noreply.github.com>
2020-11-12 16:51:36 -0600

    Studio chapter 14 complete

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+[Route("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user/")]
+        public IActionResult SubmitAddUserForm(User newUser , string varify)
+        {
+            if (newUser.Password == varify)
+            {
+                ViewBag.SpaUser = newUser.UserName;
+                ViewBag.email = newUser.Email;
+                ViewBag.date = newUser.Date;
+                return View("Index");
+            }
+
+            else
+            {
+                ViewBag.username = newUser.UserName;
+               
+                return View("Add");
+            }
+           
+        }
+    }
+    
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime Date { get; }
+
+        public User() // need to keep this no arg constructor. otherwise  it can not pick valu from input type text form Add.cshtml it will try to find a null constructor.
+        {
+            Date = DateTime.Now;
+        }
+        public User(string userName, string email, string password): this()
+        {
+            UserName = userName;
+            Email = email;
+            Password = password;
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
